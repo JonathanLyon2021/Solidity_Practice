12798e19617b650b2820053a8e80f12d74c7c0ab
Jonathan Lyon <82686765+JonathanLyon2021@users.noreply.github.com>
2022-08-14 23:58:26 -0400

    Create Enums.sol

---
+++ Enums.sol
@@ -0,0 +1,9 @@
+pragma solidity ^0.5.0;
+
+contract test {
+   enum FreshJuiceSize{ SMALL, MEDIUM, LARGE }
+   FreshJuiceSize choice;
+   FreshJuiceSize constant defaultChoice = FreshJuiceSize.MEDIUM;
+
+ 
+}
