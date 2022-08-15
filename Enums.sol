5e59a1bd9fd26e097a4de980c27afd051cb99018
Jonathan Lyon <82686765+JonathanLyon2021@users.noreply.github.com>
2022-08-14 23:58:40 -0400

    Update Enums.sol

--- Enums.sol
+++ Enums.sol
@@ -5,5 +5,14 @@ contract test {
    FreshJuiceSize choice;
    FreshJuiceSize constant defaultChoice = FreshJuiceSize.MEDIUM;
 
+    function setLarge() public {
+      choice = FreshJuiceSize.LARGE;
+   }
+   function getChoice() public view returns (FreshJuiceSize) {
+      return choice;
+   }
+   function getDefaultChoice() public pure returns (uint) {
+      return uint(defaultChoice);
+   }
  
 }
