1a49a1f141eefb987930e5789ed549ddccc82cc3
Jonathan Lyon <82686765+JonathanLyon2021@users.noreply.github.com>
2022-08-15 00:27:10 -0400

    Added Contract Register is Owner{}

--- FunctionModifiers.sol
+++ FunctionModifiers.sol
@@ -29,3 +29,15 @@ contract Owner {
       }
    }
 }
+contract Register is Owner {
+   mapping (address => bool) registeredAddresses;
+   uint price;
+   constructor(uint initialPrice) public { price = initialPrice; }
+   
+   function register() public payable costs(price) {
+      registeredAddresses[msg.sender] = true;
+   }
+   function changePrice(uint _price) public onlyOwner {
+      price = _price;
+   }
+}
