c1b18c5570a0e44f2f809087a3353eedde1247d0
Jonathan Lyon <82686765+JonathanLyon2021@users.noreply.github.com>
2022-08-19 15:59:09 -0400

    Create CryptoKids.sol

---
+++ CryptoKids.sol
@@ -0,0 +1,15 @@
+// SPDX-License-Identifier: Unlicensed
+
+pragma solidity ^0.8.7;
+
+contract CryptoKids {
+        //owner Dad
+
+        //define Kid
+
+        //add kid to the contract, specifically to a kids account
+
+        //kid checks if able to withdraw
+
+        //withdraw money
+}
